88062e8ae2bb8627e0da27408ba2694642f51d3f
Sergey Zhukov <szhukov@aligntech.com>
2024-10-19 16:25:02 +0300

    [Sprint 2] fix: solution add more tests to email module

--- auth-service/src/domain/email.rs
+++ auth-service/src/domain/email.rs
@@ -20,17 +20,42 @@ impl AsRef<str> for Email {
 mod tests {
     use super::*;
 
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
     #[test]
-    fn test_valid_parse_email() {
-        let input = "email@com";
-        let res = Email::parse(input).unwrap();
-        assert_eq!(res.0, input);
+    fn empty_string_is_rejected() {
+        let email = "";
+        let res = Email::parse(email);
+        assert!(res.is_err());
     }
 
     #[test]
-    fn test_invalid_parse_email() {
-        let input = "emailcom";
-        let res = Email::parse(input);
-        assert_eq!(res, Err(format!("failed parsing email: {}", input)));
+    fn test_valid_parse_email() {
+        let email = "email@com";
+        let res = Email::parse(email);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "emailcom";
+        let res = Email::parse(email);
+        assert!(res.is_err());
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        Email::parse(&valid_email.0).is_ok()
     }
 }
