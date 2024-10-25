010c0a2b7ba78ca3fd0e17f991f2ee7a7084a459
Sergey Zhukov <szhukov@aligntech.com>
2024-10-25 17:34:27 +0300

    [Sprint 3] fix: Solution tests for signup, verify_token

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,37 +1,5 @@
-use crate::helpers::{get_random_email, TestApp};
+use crate::helpers::TestApp;
 use auth_service::{routes::SignupResponse, ErrorResponse};
-
-#[tokio::test]
-async fn should_return_422_if_malformed_input() {
-    let app = TestApp::new().await;
-
-    let random_email = get_random_email();
-
-    let test_cases = [
-        serde_json::json!({
-            "password": "password123",
-            "requires2FA": true
-        }),
-        serde_json::json!({
-            "password": "password1",
-            "requires2FA": true
-        }),
-        serde_json::json!({
-            "password": "foo",
-            "requires2FA": true
-        }),
-    ];
-
-    for test_case in test_cases.iter() {
-        let response = app.post_signup(test_case).await;
-        assert_eq!(
-            response.status().as_u16(),
-            422,
-            "Failed for input: {:?}",
-            test_case
-        );
-    }
-}
 
 #[tokio::test]
 async fn should_return_201_if_valid_input() {
@@ -58,7 +26,6 @@ async fn should_return_201_if_valid_input() {
         expected_response
     );
 }
-
 #[tokio::test]
 async fn should_return_400_if_invalid_input() {
     let app = TestApp::new().await;
@@ -120,4 +87,34 @@ async fn should_return_409_if_email_already_exists() {
             .error,
         "User already exists".to_owned()
     );
+}
+
+#[tokio::test]
+async fn should_return_422_if_malformed_input() {
+    let app = TestApp::new().await;
+
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "password": "password1",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "password": "foo",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
